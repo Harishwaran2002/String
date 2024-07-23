91984398160bafaf52cfcb15f1c229e52c1e4d0b
Harishwaran2002 <146076518+Harishwaran2002@users.noreply.github.com>
2024-07-23 12:55:42 +0530

    String.java

---
+++ String.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    }
+}
